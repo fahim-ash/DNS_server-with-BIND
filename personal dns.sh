aec3c1bcc9baa9af94f9475c6b12ec930ce30ffa
fahim ashhab <37286610+fahim-ash@users.noreply.github.com>
2021-04-27 21:16:20 +0600

    Update personal dns.sh

--- personal dns.sh
+++ personal dns.sh
@@ -3,70 +3,6 @@
 yum -y install bind bind-utils
 
 cat > /etc/named.conf <<eof
-// See the BIND Administrator's Reference Manual (ARM) for details about the
-// configuration located in /usr/share/doc/bind-{version}/Bv9ARM.html
-
-options {
-        listen-on port 53 { 127.0.0.1;192.168.0.104; };
-        listen-on-v6 port 53 { ::1; };
-        directory       "/var/named";
-        dump-file       "/var/named/data/cache_dump.db";
-        statistics-file "/var/named/data/named_stats.txt";
-        memstatistics-file "/var/named/data/named_mem_stats.txt";
-        recursing-file  "/var/named/data/named.recursing";
-        secroots-file   "/var/named/data/named.secroots";
-        allow-query     { any; };
-
-        /*
-         - If you are building an AUTHORITATIVE DNS server, do NOT enable recursion.
-         - If you are building a RECURSIVE (caching) DNS server, you need to enable
-           recursion.
-         - If your recursive DNS server has a public IP address, you MUST enable access
-           control to limit queries to your legitimate users. Failing to do so will
-           cause your server to become part of large scale DNS amplification
-           attacks. Implementing BCP38 within your network would greatly
-           reduce such attack surface
-        */
-        recursion yes;
-
-        dnssec-enable yes;
-        dnssec-validation yes;
-
-        /* Path to ISC DLV key */
-        bindkeys-file "/etc/named.root.key";
-
-        managed-keys-directory "/var/named/dynamic";
-
-        pid-file "/run/named/named.pid";
-        session-keyfile "/run/named/session.key";
-};
-
-logging {
-        channel default_debug {
-                file "data/named.run";
-                severity dynamic;
-        };
-};
-
-zone "." IN {
-        type hint;
-        file "named.ca";
-};
-
-include "/etc/named.rfc1912.zones";
-include "/etc/named.root.key";
-eof
-
-#start the service
-
-systemctl start named
-systemctl enable named
-
-firewall-cmd --permanent --add-port=53/tcp
-firewall-cmd --permanent --add-port=53/udp
-firewall-cmd --reload
-
-cat > /etc/named.conf <<eof
 //
 // named.conf
 //
@@ -178,6 +114,19 @@ client  IN      A       192.168.0.105
 105     IN      PTR     desktop.com.
 
 eof
+
+
+
+#start the service
+
+systemctl start named
+systemctl enable named
+
+firewall-cmd --permanent --add-port=53/tcp
+firewall-cmd --permanent --add-port=53/udp
+firewall-cmd --reload
+
+
 
 chown root:named /var/named/forward.ash.com
 chown root:named /var/named/reverse.ash.com
